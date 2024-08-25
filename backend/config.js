371606dfe8f68472e62473493a41b8499243a3da
awangran <ashleero.yin@gmail.com>
2024-08-25 18:20:31 -0500

    packages

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://awangran:5fV29oPb7mJupE1y@letmecook.vtgsr.mongodb.net/?retryWrites=true&w=majority&appName=letmecook'
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { mongoDBURL, PORT } from "./config.js"
+import mongoose from 'mongoose'
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is running in port: ${PORT}`)
-});
 
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('app conected to db');
+        app.listen(PORT, () => {
+            console.log(`App is running in port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
