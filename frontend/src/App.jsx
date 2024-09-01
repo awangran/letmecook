f506d48e374b70ac574ece2d04881cca7277c2aa
awangran <ashleero.yin@gmail.com>
2024-09-01 12:40:07 -0500

    routes work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { useState } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home'
+import Root from './routes/Root'
+import Fridge from './routes/Fridge'
+import Recipes from './routes/Recipes'
+import Search from './routes/Search'
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-    
-    
+      <Route path='/' element={<Root />} />
+      <Route path='/fridge' element={<Fridge />} />
+      <Route path='/recipes' element={<Recipes />} />
+      <Route path='/search' element={<Search />} />
     </Routes>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, RouterProvider } from 'react-router-dom'
 import { extendTheme } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
@@ -18,10 +18,10 @@ const theme = extendTheme({ colors })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <Router>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+    </Router>
   </StrictMode>
 )
