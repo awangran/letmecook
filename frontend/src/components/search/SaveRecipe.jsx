83bc2539bfd08c4aff7e544f14eceb9a54e3ea8e
awangran <ashleero.yin@gmail.com>
2025-01-13 16:25:08 -0500

    canmake

---
+++ frontend/src/components/search/SaveRecipe.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function SaveRecipe() {
+  return (
+    <>
+    <Flex
+        width='100%'
+        height='100vh' 
+        position='absolute' 
+        top='0' 
+        justifyContent='center'
+        zIndex='10'
+        left='0'
+        >
+
+    </Flex>
+    </>
+  )
+}
+
+export default SaveRecipe
